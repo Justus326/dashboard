f14e2fb78cf823ee17347ecb600f2562afda2f75
Justus Ejiofor <justusejiofor326@gmail.com>
2022-11-13 19:31:39 +0100

    first commit

---
+++ src/pages/Charts/Bar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Header, Stacked } from '../../components';
+
+const Bar = () => {
+  return (
+
+    <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg
+    rounded-3xl">
+     <Header category="Chart" title="Inflation Rate" />
+
+     <div className="w-full">
+       <Stacked />
+     </div>
+    </div>
+  )
+}
+
+export default Bar
